9b4d9c5f2e05be54d715cf92d91ff6afa590ef9e
Maxi <Mrinaldi2204@gmail.com>
2024-11-29 11:47:44 -0300

    Add more space

--- src/components/TrabajosMain.tsx
+++ src/components/TrabajosMain.tsx
@@ -19,7 +19,7 @@ export function TrabajosMain() {
   });
 
   return (
-    <section className="bg-white py-10">
+    <section className="bg-white py-28">
       <Margin>
         <div
           data-aos="fade-right"
